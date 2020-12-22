6dc1781845f7dfaea0e5530068f138b051318d30
Na Ngo <na.np@neo-lab.vn>
2020-12-22 17:34:24 +0700

    !update data

--- data/generate.py
+++ data/generate.py
@@ -1,8 +1,16 @@
 import os
 import json
+import blurhash
 
 arr = os.listdir('./meme_data')
-json_string = json.dumps(arr)
-f = open("huge_pack.json", "a")
+arr_result = []
+for item in arr:
+    print(item)
+    if item != '.DS_Store':
+        with open('./meme_data/'+ item, 'r') as image_file:
+            hash = blurhash.encode(image_file, x_components=4, y_components=3)
+            arr_result.append({'image':'https://nielsezeka.github.io/data/meme_data/'+ item, 'blur_hash': hash});
+json_string = json.dumps(arr_result)
+f = open("huge_pack_blur.json", "a")
 f.write(json_string)
 f.close()
