89564e88fbe74a92bc24e82c10ae41a6756f827b
DESKTOP-EC68343\Na Ngo <ngopona@gmail.com>
2021-08-14 16:03:55 +0700

    test on master channel

--- dota2_master/flutter_service_worker.js
+++ dota2_master/flutter_service_worker.js
@@ -6,14 +6,14 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "a50ccbdbc4daef553fb3a5bffebb4ca3",
+"assets/NOTICES": "2fedf11b908b9c7c6e8ffbbdebbdb82f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "89c2fd9f64d14730d27bf5b9a067d348",
-"/": "89c2fd9f64d14730d27bf5b9a067d348",
-"main.dart.js": "ed502b1565a4b2def9a07037dd9bbf18",
+"index.html": "73da9a72272b362a8bdedcadd7c4e138",
+"/": "73da9a72272b362a8bdedcadd7c4e138",
+"main.dart.js": "001c854bcc193c16851b2bdedaff57df",
 "manifest.json": "1d4b9d499d2acf23ec685df60b50afae",
 "version.json": "9230479c8beafb78a93dff199ecfed65"
 };
