b3465548dff8fab664c4ff00b837548edf2d70b0
Na Ngo <ngopona@gmail.com>
2022-03-19 20:05:14 +0700

    ! remove old code and update new code

--- data/generate.py
+++ data/generate.py
@@ -1,16 +1,14 @@
 import os
 import json
-import blurhash
 
-arr = os.listdir('./meme_data')
+arr = os.listdir('./meme_data_2')
 arr_result = []
 for item in arr:
     print(item)
     if item != '.DS_Store':
-        with open('./meme_data/'+ item, 'r') as image_file:
-            hash = blurhash.encode(image_file, x_components=4, y_components=3)
-            arr_result.append({'image':'https://nielsezeka.github.io/data/meme_data/'+ item, 'blur_hash': hash});
+        with open('./meme_data_2/'+ item, 'r') as image_file:
+            arr_result.append({'image':'https://nielsezeka.github.io/data/meme_data_2/'+ item});
 json_string = json.dumps(arr_result)
-f = open("huge_pack_blur.json", "a")
+f = open("huge_pack_pexel.json", "a")
 f.write(json_string)
 f.close()
